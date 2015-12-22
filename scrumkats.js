5bd1d3110102d22e17439fbd89db7dcda747af0b
k9ordon <github@k94n.com>
2015-12-22 10:04:49 +0100

    minor fixes

--- scrumkats.js
+++ scrumkats.js
@@ -1,64 +1,60 @@
 var cards = [{
-  number: .5
-},{
-  number: 1
+    number: "0"
 }, {
-  number: 2
+    number: .5
 }, {
-  number: 3
+    number: 1
 }, {
-  number: 4
+    number: 2
 }, {
-  number: 5
+    number: 3
 }, {
-  number: 6
+    number: 5
 }, {
-  number: 7
+    number: 8
+}, {
+    number: 13
+}, {
+    number: 20
+}, {
+    number: 40
+}, {
+    number: "?"
+}, {
+    number: "∞"
 }];
 
 if (Meteor.isClient) {
-  Session.setDefault('current', false);
-  Session.setDefault('flipped', false);
+    Session.setDefault('current', false);
+    Session.setDefault('flipped', false);
 
-  Template.board.helpers({
-    cards: cards,
-    hasActiveCardClassName: function() {
-      return Session.get('current') ? 'hasActiveCard' : '';
-    }
-  });
+    Template.board.helpers({
+        cards: cards,
+        hasActiveCardClassName: function() {
+            return Session.get("current") ? "hasActiveCard" : ""
+        }
+    });
 
-  Template.board.events({
-    'click .board': function(e) {
-      console.log('clicked board');
+    Template.board.events({
+        "click .board": function(e) {
+            return console.log("clicked board"), Session.set("current", !1), Session.set("flipped", !1), !1
+        }
+    });
 
-      Session.set('current', false);
-      Session.set('flipped', false);
-
-      return false;
-    }
-  });
-
-  Template.card.helpers({
-    activeClassName: function() {
-      return (this.number === Session.get('current')) ? 'active' : '';
-    },
-    flippedClassName: function() {
-      return (this.number === Session.get('current') && Session.get('flipped')) ? 'flipped' : '';
-    }
-  });
-
-  Template.card.events({
-    'click .card:not(.active)': function(e) {
-      //$el = e.target;
-      //console.log('clicked card', $el.getBoundingClientRect(), this.number);
-
-      Session.set('current', this.number);
-      Session.set('flipped', false);
-      return false;
-    },
-    'click .card.active': function(e) {
-      Session.set('flipped', Session.get('flipped') ? false : true);
-      return false;
-    }
-  });
+    Template.card.helpers({
+        activeClassName: function() {
+            return this.number === Session.get("current") ? "active" : ""
+        },
+        flippedClassName: function() {
+            return this.number === Session.get("current") && Session.get("flipped") ? "flipped" : ""
+        }
+    });
+    Template.card.events({
+        "click .card:not(.active)": function(e) {
+            return Session.set("current", this.number), Session.set("flipped", !1), !1
+        },
+        "click .card.active": function(e) {
+            return Session.set("flipped", Session.get("flipped") ? !1 : !0), !1
+        }
+    });
 }
