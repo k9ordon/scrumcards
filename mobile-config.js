4966de537bb36de9c7ce8abb2293859d1b5b1b8e
kschreiber <klemens.schreiber@karriere.at>
2015-12-29 21:40:06 +0100

    phonegap init

---
+++ mobile-config.js
@@ -0,0 +1,19 @@
+// This section sets up some basic app metadata,
+// the entire section is optional.
+App.info({
+  // id: 'com.example.matt.uber',
+  name: 'Scrum Cards',
+  description: 'scrum grooming cards. groom groom.',
+  author: 'k9ordon',
+  email: 'gordon@k94n.com',
+  website: 'http://github.com/k9ordon/scrumcards'
+});
+
+
+App.setPreference('BackgroundColor', '0xffffffff');
+App.setPreference('HideKeyboardFormAccessoryBar', true);
+App.setPreference('Orientation', 'default');
+App.setPreference('Orientation', 'all', 'ios');
+
+App.setPreference('StatusBarOverlaysWebView', 'true');
+App.setPreference('StatusBarBackgroundColor', '#FBFBFB');
--- view/layout.js
+++ view/layout.js
@@ -1,8 +1,16 @@
 if (Meteor.isClient) {
-    Template.layout.helpers({
-        userId: function() {
-            return superheros[Session.get("userId")]
-        }
-    });
-    Template.layout.events({});
+  Template.layout.helpers({
+    userId: function() {
+      return superheros[Session.get("userId")]
+    },
+    platformClassName: function() {
+      if (Meteor.isCordova) {
+        return "platform-cordova";
+      }
+      if (Meteor.isClient) {
+        return "platform-web";
+      }
+    }
+  });
+  Template.layout.events({});
 }
