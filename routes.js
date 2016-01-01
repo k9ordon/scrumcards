9bf7d88910837cc82298da97aba026f498a100cb
kschreiber <klemens.schreiber@karriere.at>
2016-01-01 22:17:44 +0100

    rewrote route

--- routes.js
+++ routes.js
@@ -1,41 +1,73 @@
-Router.route('/', function() {
-    this.render('home');
+Router.route('/', {
+  name: 'home'
 });
 
-Router.route('/:boardSlug', function() {
+Router.route('/:boardSlug', {
+  name: 'board',
+  subscriptions: function() {
+    var boardSlug = this.params.boardSlug.toLowerCase();
+
+    this.subscribe('boardUsers', boardSlug),
+      this.subscribe('board', boardSlug).wait();
+  },
+  onRun: function() {
     var boardSlug = this.params.boardSlug.toLowerCase();
 
     console.log('boardSlug', boardSlug);
 
     Session.set('boardSlug', boardSlug);
 
-    this.wait([
-        Meteor.subscribe('board', boardSlug),
-        Meteor.subscribe('boardUsers', boardSlug)
-    ]);
+    Meteor.call("enterBoard", Session.get('userId'), boardSlug, Session.get('theme'));
 
-    if (this.ready()) {
-        Meteor.call("enterBoard", Session.get('userId'), boardSlug, Session.get('theme'));
+    Meteor.call("heartbeat", Session.get('userId'), boardSlug);
+    Meteor.setInterval(function() {
+      Meteor.call("heartbeat", Session.get('userId'), boardSlug);
+    }, (30 * 1000));
 
-        // Meteor.setInterval(function() {
-        //     Meteor.call("heartbeat", Session.get('userId'), boardSlug);
-        // }, (30*1000));
-    }else {
-        console.log('not ready', boardSlug);
-    }
+    this.next();
+  },
+  onStop: function() {
+    var boardSlug = this.params.boardSlug.toLowerCase();
+    console.log('boardSlug exit', boardSlug);
 
+    Meteor.call("leaveBoard",
+        Session.get('userId'),
+        boardSlug
+    );
+  },
+  data: function() {
+    var boardSlug = this.params.boardSlug.toLowerCase();
     var board = Boards.findOne({
-        slug: boardSlug
+      slug: boardSlug
     });
 
-    this.render('board', {
-        data: {
-            boardSlug: boardSlug,
-            board: board,
-        }
-    });
+    return {
+      boardSlug: boardSlug,
+      board: board
+    };
+  }
 });
+//
+// Router.route('/:boardSlug', function() {
+//
+//     if (this.ready()) {
+//
+//     }else {
+//         console.log('not ready', boardSlug);
+//     }
+//
+//     var board = Boards.findOne({
+//         slug: boardSlug
+//     });
+//
+//     this.render('board', {
+//         data: {
+//             boardSlug: boardSlug,
+//             board: board,
+//         }
+//     });
+// });
 
 Router.route('/card', function() {
-    this.render('card');
+  this.render('card');
 });
